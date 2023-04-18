416c50fc9ae5fa70448590717a2005d1873850a7
TANJIL87 <93540110+TANJIL87@users.noreply.github.com>
2023-04-18 21:44:59 +0600

    Add files via upload
    Signed-off-by: TANJIL87 <93540110+TANJIL87@users.noreply.github.com>

--- convert_icons.sh
+++ convert_icons.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-# Install Pillow
-pip install pillow
+# Install necessary dependencies
+sudo apt-get update
+sudo apt-get install -y imagemagick
 
-# Check if the input and output directories exist, create them if they don't
-if [ ! -d "input" ]; then
-  echo "Error: input directory does not exist"
-  exit 1
-fi
-if [ ! -d "output" ]; then
-  mkdir output
-fi
+# Download and extract the resource pack
+wget -O resources.zip $1
+unzip -o resources.zip -d resources
 
-# Define the function to convert an image
-function convert_image {
-  # Get the input and output filenames
-  input_file="$1"
-  output_file="$2"
-  # Load the image and convert it to a 2D icon
-  python - <<END
-from PIL import Image
+# Loop through each item icon in the 3D assets directory
+for file in resources/assets/minecraft/textures/item/*.png; do
+  # Get the filename without extension
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
 
-input_image = Image.open("$input_file")
-output_image = input_image.crop((0, 0, 16, 16))
-output_image.save("$output_file")
-END
-}
-
-# Loop over all files in the input directory
-for input_file in input/*.png; do
-  # Get the filename without the extension
-  filename=$(basename "$input_file" .png)
-  # Convert the image and save it to the output directory
-  convert_image "$input_file" "output/$filename.png"
+  # Convert the 3D icon to a 2D icon using ImageMagick
+  convert "$file" -crop 64x64+0+96 "output/${filename}.png"
 done
+
+# Zip the output folder
+zip -r output.zip output
+
+# Clean up
+rm -rf resources.zip resources output
